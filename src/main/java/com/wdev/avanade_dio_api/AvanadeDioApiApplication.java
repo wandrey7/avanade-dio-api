fabd938ebd84d3e3bfe11b6fdc17b438e5a4bd1f
wandrey7 <pedroxavier533@gmail.com>
2025-03-01 11:19:58 -0300

    first commit

---
+++ src/main/java/com/wdev/avanade_dio_api/AvanadeDioApiApplication.java
@@ -0,0 +1,13 @@
+package com.wdev.avanade_dio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeDioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeDioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdev/avanade_dio_api/AvanadeDioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdev.avanade_dio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeDioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

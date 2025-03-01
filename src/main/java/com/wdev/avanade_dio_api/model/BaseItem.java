24456074c6f02014dd4766baa1818f42d3609d06
wandrey7 <pedroxavier533@gmail.com>
2025-03-01 17:10:51 -0300

    fix(baseitemmodel): correct datatype mapping

--- src/main/java/com/wdev/avanade_dio_api/model/BaseItem.java
+++ src/main/java/com/wdev/avanade_dio_api/model/BaseItem.java
@@ -14,11 +14,11 @@ public abstract class BaseItem {
     private String icon;
     private String description;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
